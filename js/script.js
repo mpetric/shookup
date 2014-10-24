51adf4ffe3e469498f9bda9108faafbd509f3fd3
mpetric <matt.petric@gmail.com>
2014-10-23 20:17:28 -0400

    First Commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$("#leftleg").click(function(){
+        $("#leftleg").css("height", 50 + Math.floor(Math.random() * 75));
+});
+
+$("#rightleg").click(function(){
+        $("#rightleg").css("height", 50 + Math.floor(Math.random() * 75));
+});
+
+var value =  0;
+$("#image").rotate({ 
+   bind: 
+     { 
+        click: function(){
+            value +=Math.floor(Math.random() * 360);
+            $(this).rotate({ animateTo:value})
+        }    
+     } 
+});
